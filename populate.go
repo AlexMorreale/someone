7ded0a1d7feab3ccd7b5b57e16ab17660800b002
AlexMorreale <alexmorreale@me.com>
2018-04-23 11:28:02 -0400

    remove init content

--- populate.go
+++ populate.go
@@ -1,7 +1,4 @@
 package main
 
 func init() {
-	CreateQuote(Quote{Text: "Everyone on my team is 'pre-fired'"})
-	//CreateQuote(Quote{Text: "Are you happy?"})
-	//CreateQuote(Quote{Text: "Don't let me touch Kubernetes, i'll blue green destroy it."})
 }
